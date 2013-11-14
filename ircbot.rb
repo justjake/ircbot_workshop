a892f167eb5aa19123e967e22bc842aae9312eb2
Jake Teton-Landis <just.1.jake@gmail.com>
2013-11-13 17:37:48 -0800

    level2 end

--- ircbot.rb
+++ ircbot.rb
@@ -13,7 +13,8 @@ require 'cinch'
 
 # some global constants - plz don't remove the ENV['USER'] part
 # during this workshop. It's good to know who is who.
-BOT_NICK =    ENV['USER'] + '-rudewatcher'
+BOT_TYPE = 'rudewatcher'
+BOT_NICK =    ENV['USER'] + '-' + BOT_TYPE
 BOT_CHANNELS = ['#ircbots']
 
 # bot responds to '<Bot Name>: ', in channels, or commands at the start of the line, in PMs
@@ -43,16 +44,52 @@ class BotActions
   # use_prefix: true indicates that we only match messages sent to our bot
   # which is to say prefixed with our bot's name:
   # 12:04 <@jitl>: josephz-rudewatcher: thank you for telling me I was rude.
-  match /YOUR REGEX HERE/, use_prefix: true, method: :on_reply
+  match /thank|shut up|wow|stop|annoy|job/, use_prefix: true, method: :on_reply
+
+  EXCLAMATIONS = [
+    'wow',
+    'jeezy peets',
+    'holy cow',
+    'nhyomygawd',
+    '(!)'
+  ]
+
+  RUDE_WORDS = [
+    'poop',
+    'butt',
+    'moist',
+    'fart',
+    'miscreant',
+    'ruffian',
+    'harlot',
+    'your mom',
+  ]
 
   def on_message(message)
-    # check for rude words
-    # then respond
+    # ignore bots of same type
+    if message.user.nick.include? BOT_TYPE
+      return
+    end
+
+    # accumulate the rude words we see in the message
+    rude_words_used = []
+    RUDE_WORDS.each do |word|
+      if message.message.downcase.include? word
+        rude_words_used.push(word)
+      end
+    end
+
+    # send reply if any rude words were used
+    if rude_words_used.length == 1
+      message.reply("#{EXCLAMATIONS.sample} #{message.user}, you just used a pretty rude word")
+    elsif rude_words_used.length > 1
+      message.reply("#{EXCLAMATIONS.sample} #{message.user}, you just used #{rude_words_used.length} rude words.")
+    end
   end
 
   def on_reply(message)
-    # someone said "thanks" or related
     puts "Message matched reply regexp: #{message.to_s}"
+    message.reply("you're welcome ^_^")
   end
 
 end
